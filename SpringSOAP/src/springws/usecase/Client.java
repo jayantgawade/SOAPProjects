d8b771ed17147c3d5a8366be216c8f7ebaa32049
Jayant <jayantg@pragiti.com>
2016-04-05 13:24:58 +0530

    WSDL Generation

---
+++ SpringSOAP/src/springws/usecase/Client.java
@@ -0,0 +1,27 @@
+package springws.usecase;
+
+import java.io.StringReader;
+
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+public class Client {
+	
+	// sample request XML
+	private static String MESSAGE = "<orderRequest xmlns=\"http://www.springwssample.org/types\"><item>Spring Flowers</item><quantity>2</quantity><city>Ahmedabad</city><country>India</country></orderRequest>";
+
+	public static void main(String[] args) {
+		StreamSource source = new StreamSource(new StringReader(MESSAGE));
+		StreamResult result = new StreamResult(System.out);
+
+		// WebServiceTemplate provides the functionality for sending and
+		// receiving webservice messages.
+		WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+		webServiceTemplate.sendSourceAndReceiveToResult(
+				"http://localhost:7080/SpringSOAP/WebContent/orderservices", source,
+				result);
+
+	}
+}
